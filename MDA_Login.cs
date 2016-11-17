045d2fa4c9c6213fb2a7fd5d3b59d25167cac032
Vinish Abraham <VinishAbraham@users.noreply.github.com>
2016-11-17 11:23:10 -0600

    Add files via upload

---
+++ MDA_Login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Launch MDA in Chrome Incognito
+
+            var options = new ChromeOptions();
+            options.AddArgument("incognito");     
+            IWebDriver driver = new ChromeDriver(@"C:\libraries\",options);
+            driver.Url = "https://usnconeboxax1aos.cloud.onebox.dynamics.com/";
+
+
+            #endregion
+
+
+
+
+
+
+        }
+    }
+}
